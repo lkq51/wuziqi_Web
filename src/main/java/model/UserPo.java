8268d8048af0b7dcc8af0d8fcf9a21f6a7fed252
lkq <626883409@qq.com>
2016-10-11 22:40:31 +0800

    Initial commit

---
+++ src/main/java/model/UserPo.java
@@ -0,0 +1,79 @@
+package model;
+
+import javax.persistence.*;
+
+/**
+ * Created by lkq on 2016/10/11.
+ */
+@Entity
+@Table(name = "user", schema = "wuziqi")
+public class UserPo {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "username", nullable = false, length = 20)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 30)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, length = 120)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserPo userPo = (UserPo) o;
+
+        if (id != userPo.id) return false;
+        if (username != null ? !username.equals(userPo.username) : userPo.username != null) return false;
+        if (password != null ? !password.equals(userPo.password) : userPo.password != null) return false;
+        if (email != null ? !email.equals(userPo.email) : userPo.email != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lkq
+  Date: 2016/10/10
+  Time: 14:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>五子棋游戏--登录</title>
+  </head>
+  <body>
+    <div>
+      <form id="loginForm" action="<%=pageContext %>/form/loginAction" method="post">
+        用户名：<input id="username" type="text"><br>
+        密码：  <input id="password" type="password"><br>
+        <input type="submit" value="登录">
+      </form>
+    </div>
+  </body>
+</html>
