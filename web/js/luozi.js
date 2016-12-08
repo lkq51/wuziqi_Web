cef8238d078b7537234161ff86e1a09b6bd6031b
lou <626883409@qq.com>
2016-12-08 18:00:50 +0800

    Merge branch 'master' of https://github.com/lkq51/wuziqi_Web
    # Conflicts:
    #	src/main/java/model/UserPo.hbm.xml
    #	src/main/java/model/UserPo.java
    #	wuziqi_web.iml

--- web/js/luozi.js
+++ web/js/luozi.js
@@ -1,35 +1,35 @@
 /**
  * Created by lkq on 2016/11/28.
  */
-   var event,
-       BLACK = "black",
-       WHITE = "white",
-       winner = "",         //胜利者
-       whoPlay = BLACK,     //轮到哪一方下棋了
-       turn = 0,            //当前进行的回合数=turn/2
-       moveFirst = BLACK, //哪种颜色先下
-       color = WHITE,     //棋子当前显示的颜色
-       lastPosition,
-       qizis = new Array(),//存储整个棋盘棋子分布的数组
-       canvasSize = 750,    //画布大小
-       grid = 15,           //棋盘大小为grid * grid
-       chessSize = 20,   //棋子的半径
-       chessboardSize = 700, //棋盘的边长
-       extra = (canvasSize - chessboardSize) / 2,  //棋盘与画布的空余
-       interval=chessboardSize / (grid-1); //每个格子的边长
-   for(var i = 0;i<grid;i++){
-       qizis[i] = new Array();
-   }
+var event,
+    BLACK = "black",
+    WHITE = "white",
+    winner = "",         //胜利者
+    whoPlay = BLACK,     //轮到哪一方下棋了
+    turn = 0,            //当前进行的回合数=turn/2
+    moveFirst = BLACK, //哪种颜色先下
+    color = WHITE,     //棋子当前显示的颜色
+    lastPosition,
+    qizis = new Array(),//存储整个棋盘棋子分布的数组
+    canvasSize = 750,    //画布大小
+    grid = 15,           //棋盘大小为grid * grid
+    chessSize = 20,   //棋子的半径
+    chessboardSize = 700, //棋盘的边长
+    extra = (canvasSize - chessboardSize) / 2,  //棋盘与画布的空余
+    interval = chessboardSize / (grid - 1); //每个格子的边长
+for (var i = 0; i < grid; i++) {
+    qizis[i] = new Array();
+}
 function getCanvasPos(canvas) {
     var rect = canvas.getBoundingClientRect();
-    return{
+    return {
         x: event.clientX - rect.left * (canvas.width / rect.width),
         y: event.clientY - rect.top * (canvas.height / rect.height)
     };
 }
 function getqiziPosition() {
     var clickPosition = getCanvasPos(qipan);
-    if(clickPosition.x>(extra+chessboardSize)&&clickPosition.x<extra&&clickPosition.y<extra&&clickPosition.y>(extra+chessboardSize)){
+    if (clickPosition.x > (extra + chessboardSize) && clickPosition.x < extra && clickPosition.y < extra && clickPosition.y > (extra + chessboardSize)) {
         return null;
     }
     clickPosition.x = clickPosition.x - extra;
@@ -37,23 +37,23 @@ function getqiziPosition() {
     var x = parseInt(clickPosition.x / interval),
         y = parseInt(clickPosition.y / interval),
         points = [];
-        points[0] = {x: x * interval, y: y * interval};
-        points[1] = {x: (x + 1) * interval, y: y * interval};
-        points[2] = {x: x * interval, y: (y + 1) * interval};
-        points[3] = {x: (x + 1) * interval, y: (y + 1) * interval};
-        var distances = [];
-        distances[0] = Math.sqrt(Math.pow((clickPosition.x - points[0].x), 2) + Math.pow((clickPosition.y - points[0].y), 2));
-        distances[1] = Math.sqrt(Math.pow((points[1].x - clickPosition.x), 2) + Math.pow((clickPosition.y - points[1].y), 2));
-        distances[2] = Math.sqrt(Math.pow((clickPosition.x - points[2].x), 2) + Math.pow((points[2].y - clickPosition.y), 2));
-        distances[3] = Math.sqrt(Math.pow((points[3].x - clickPosition.x), 2) + Math.pow((points[3].y - clickPosition.y), 2));
-        lowest = 0;
-    for(var i = 0;i<distances.length;i++){
-        if (distances[i]<distances[lowest])
+    points[0] = {x: x * interval, y: y * interval};
+    points[1] = {x: (x + 1) * interval, y: y * interval};
+    points[2] = {x: x * interval, y: (y + 1) * interval};
+    points[3] = {x: (x + 1) * interval, y: (y + 1) * interval};
+    var distances = [];
+    distances[0] = Math.sqrt(Math.pow((clickPosition.x - points[0].x), 2) + Math.pow((clickPosition.y - points[0].y), 2));
+    distances[1] = Math.sqrt(Math.pow((points[1].x - clickPosition.x), 2) + Math.pow((clickPosition.y - points[1].y), 2));
+    distances[2] = Math.sqrt(Math.pow((clickPosition.x - points[2].x), 2) + Math.pow((points[2].y - clickPosition.y), 2));
+    distances[3] = Math.sqrt(Math.pow((points[3].x - clickPosition.x), 2) + Math.pow((points[3].y - clickPosition.y), 2));
+    lowest = 0;
+    for (var i = 0; i < distances.length; i++) {
+        if (distances[i] < distances[lowest])
             lowest = i;
     }
-    alert(points[lowest].x+"    "+points[lowest].y);
-    points[lowest].x = parseInt(points[lowest].x / interval );
-    points[lowest].y = parseInt(points[lowest].y / interval );
+    //alert(points[lowest].x + "    " + points[lowest].y);
+    points[lowest].x = parseInt(points[lowest].x / interval);
+    points[lowest].y = parseInt(points[lowest].y / interval);
     return points[lowest];
 }
 //在页面上显示出落子
@@ -62,7 +62,7 @@ function draw(qiziPosition) {
         context = qipan.getContext("2d");
     context.fillStyle = color;
     context.beginPath();
-    context.arc(qiziPosition.x * interval +extra, qiziPosition.y * interval + extra, chessSize, 0, Math.PI * 2, true);
+    context.arc(qiziPosition.x * interval + extra, qiziPosition.y * interval + extra, chessSize, 0, Math.PI * 2, true);
     context.closePath();
     context.fill();
 }
@@ -71,13 +71,13 @@ function leftBottom_rightTop(qiziPosition) {
     var num_qizi = 0,
         row = qiziPosition.x,
         col = qiziPosition.y;
-    while(row>-1&&row<grid&&col>-1&&col<grid){
-        if(qizis[row][col]!=qizis[qiziPosition.x][qiziPosition.y]){
+    while (row > -1 && row < grid && col > -1 && col < grid) {
+        if (qizis[row][col] != qizis[qiziPosition.x][qiziPosition.y]) {
             break;
-        }else {
+        } else {
             num_qizi++;
         }
-        if (row>qiziPosition.x+4||row<qiziPosition.x-4||col>qiziPosition.y+4||col<qiziPosition.y-4){
+        if (row > qiziPosition.x + 4 || row < qiziPosition.x - 4 || col > qiziPosition.y + 4 || col < qiziPosition.y - 4) {
             break;
         }
         row++;
@@ -85,13 +85,13 @@ function leftBottom_rightTop(qiziPosition) {
     }
     row = qiziPosition.x - 1;
     col = qiziPosition.y + 1;
-    while (row>-1&&row<grid&&col>-1&&col<grid){
-        if (qizis[row][col]!=qizis[qiziPosition.x][qiziPosition.y]){
+    while (row > -1 && row < grid && col > -1 && col < grid) {
+        if (qizis[row][col] != qizis[qiziPosition.x][qiziPosition.y]) {
             break;
-        }else {
+        } else {
             num_qizi++;
         }
-        if (row>qiziPosition.x + 4||row<qiziPosition.x - 4||col>qiziPosition.y + 4||col< qiziPosition.y - 4){
+        if (row > qiziPosition.x + 4 || row < qiziPosition.x - 4 || col > qiziPosition.y + 4 || col < qiziPosition.y - 4) {
             break;
         }
         row--;
@@ -104,27 +104,27 @@ function leftTop_rightBottom(qiziPosition) {
     var row = qiziPosition.x,
         col = qiziPosition.y,
         num_qizi = 0;
-    while(row>-1&&row<grid&&col>-1&&col<grid){
-        if(qizis[row][col]!=qizis[qiziPosition.x][qiziPosition.y]){
+    while (row > -1 && row < grid && col > -1 && col < grid) {
+        if (qizis[row][col] != qizis[qiziPosition.x][qiziPosition.y]) {
             break;
-        }else {
+        } else {
             num_qizi++;
         }
-        if (row>qiziPosition.x+4||row<qiziPosition.x-4||col>qiziPosition.y+4||col<qiziPosition.y-4){
+        if (row > qiziPosition.x + 4 || row < qiziPosition.x - 4 || col > qiziPosition.y + 4 || col < qiziPosition.y - 4) {
             break;
         }
         row--;
         col--;
     }
-    row = qiziPosition.x+1;
-    col = qiziPosition.y+1;
-    while(row>-1&&row<grid&&col>-1&&col<grid){
-        if(qizis[row][col]!=qizis[qiziPosition.x][qiziPosition.y]){
+    row = qiziPosition.x + 1;
+    col = qiziPosition.y + 1;
+    while (row > -1 && row < grid && col > -1 && col < grid) {
+        if (qizis[row][col] != qizis[qiziPosition.x][qiziPosition.y]) {
             break;
-        }else {
+        } else {
             num_qizi++;
         }
-        if (row>qiziPosition.x+4||row<qiziPosition.x-4||col>qiziPosition.y+4||col<qiziPosition.y-4){
+        if (row > qiziPosition.x + 4 || row < qiziPosition.x - 4 || col > qiziPosition.y + 4 || col < qiziPosition.y - 4) {
             break;
         }
         row++;
@@ -137,25 +137,25 @@ function left_right(qiziPosition) {
     var row = qiziPosition.x,
         col = qiziPosition.y,
         num_qizi = 0;
-    while(row>-1&&row<grid&&col>-1&&col<grid){
-        if(qizis[row][col]!=qizis[qiziPosition.x][qiziPosition.y]){
+    while (row > -1 && row < grid && col > -1 && col < grid) {
+        if (qizis[row][col] != qizis[qiziPosition.x][qiziPosition.y]) {
             break;
-        }else {
+        } else {
             num_qizi++;
         }
-        if (row>qiziPosition.x+4||row<qiziPosition.x-4||col>qiziPosition.y+4||col<qiziPosition.y-4){
+        if (row > qiziPosition.x + 4 || row < qiziPosition.x - 4 || col > qiziPosition.y + 4 || col < qiziPosition.y - 4) {
             break;
         }
         row--;
     }
-    row = qiziPosition.x+1;
-    while(row>-1&&row<grid&&col>-1&&col<grid){
-        if(qizis[row][col]!=qizis[qiziPosition.x][qiziPosition.y]){
+    row = qiziPosition.x + 1;
+    while (row > -1 && row < grid && col > -1 && col < grid) {
+        if (qizis[row][col] != qizis[qiziPosition.x][qiziPosition.y]) {
             break;
-        }else {
+        } else {
             num_qizi++;
         }
-        if (row>qiziPosition.x+4||row<qiziPosition.x-4||col>qiziPosition.y+4||col<qiziPosition.y-4){
+        if (row > qiziPosition.x + 4 || row < qiziPosition.x - 4 || col > qiziPosition.y + 4 || col < qiziPosition.y - 4) {
             break;
         }
         row++;
@@ -167,25 +167,25 @@ function top_bottom(qiziPosition) {
     var row = qiziPosition.x,
         col = qiziPosition.y,
         num_qizi = 0;
-    while(row>-1&&row<grid&&col>-1&&col<grid){
-        if(qizis[row][col]!=qizis[qiziPosition.x][qiziPosition.y]){
+    while (row > -1 && row < grid && col > -1 && col < grid) {
+        if (qizis[row][col] != qizis[qiziPosition.x][qiziPosition.y]) {
             break;
-        }else {
+        } else {
             num_qizi++;
         }
-        if (row>qiziPosition.x+4||row<qiziPosition.x-4||col>qiziPosition.y+4||col<qiziPosition.y-4){
+        if (row > qiziPosition.x + 4 || row < qiziPosition.x - 4 || col > qiziPosition.y + 4 || col < qiziPosition.y - 4) {
             break;
         }
         col--;
     }
-    col = qiziPosition.y+1;
-    while(row>-1&&row<grid&&col>-1&&col<grid){
-        if(qizis[row][col]!=qizis[qiziPosition.x][qiziPosition.y]){
+    col = qiziPosition.y + 1;
+    while (row > -1 && row < grid && col > -1 && col < grid) {
+        if (qizis[row][col] != qizis[qiziPosition.x][qiziPosition.y]) {
             break;
-        }else {
+        } else {
             num_qizi++;
         }
-        if (row>qiziPosition.x+4||row<qiziPosition.x-4||col>qiziPosition.y+4||col<qiziPosition.y-4){
+        if (row > qiziPosition.x + 4 || row < qiziPosition.x - 4 || col > qiziPosition.y + 4 || col < qiziPosition.y - 4) {
             break;
         }
         col++;
@@ -197,16 +197,16 @@ function ifWin(qiziPosition) {
     return left_right(qiziPosition) | leftBottom_rightTop(qiziPosition) | leftTop_rightBottom(qiziPosition) | top_bottom(qiziPosition);
 }
 function checkIfinBoard(qiziPosition) {
-    if (qiziPosition!=null&&qiziPosition.x<15&&qiziPosition.x>-1&&qiziPosition.y<15&&qiziPosition.y>-1){
+    if (qiziPosition != null && qiziPosition.x < 15 && qiziPosition.x > -1 && qiziPosition.y < 15 && qiziPosition.y > -1) {
         return true;
-    }else {
+    } else {
         return false;
     }
 }
 function checkNull(qiziPosition) {
-    if(qizis[qiziPosition.x][qiziPosition.y]!=BLACK&&qizis[qiziPosition.x][qiziPosition.y]!=WHITE){
+    if (qizis[qiziPosition.x][qiziPosition.y] != BLACK && qizis[qiziPosition.x][qiziPosition.y] != WHITE) {
         return true;
-    } else{
+    } else {
         return false;
     }
 }
@@ -215,52 +215,52 @@ function move(qiziPosition) {
     qizis[qiziPosition.x][qiziPosition.y] = color;
     draw(qiziPosition);
     turn++;
-    if(ifWin(qiziPosition)){
+    if (ifWin(qiziPosition)) {
         winner = whoPlay;
     }
-    if(whoPlay==BLACK){
+    if (whoPlay == BLACK) {
         whoPlay = WHITE;
-    }else {
+    } else {
         whoPlay = BLACK;
     }
     lastPosition = qiziPosition;
 }
 function warning() {
-    document.getElementById("winner").innerHTML="此处不能落子！！";
-    setTimeout(function(){
-        document.getElementById("winner").innerHTML="";
-    },3000);
+    document.getElementById("winner").innerHTML = "此处不能落子！！";
+    setTimeout(function () {
+        document.getElementById("winner").innerHTML = "";
+    }, 3000);
 }
 function tip() {
-       if (whoPlay == BLACK&&!winner){
-           document.getElementById("tip").innerHTML="<h1>请黑方落子</h1>";
-       }
-       if (whoPlay == WHITE&&!winner){
-           document.getElementById("tip").innerHTML="<h1>请白方落子</h1>";
-       }
+    if (whoPlay == BLACK && !winner) {
+        document.getElementById("tip").innerHTML = "<h1>请黑方落子</h1>";
+    }
+    if (whoPlay == WHITE && !winner) {
+        document.getElementById("tip").innerHTML = "<h1>请白方落子</h1>";
+    }
 }
 function gameOver() {
     document.getElementById("qipan").removeAttribute("onclick");
-    var congratulationWinner = "恭喜执"+winner+"子选手获得胜利";
+    var congratulationWinner = "恭喜执" + winner + "子选手获得胜利";
     document.getElementById("winner").innerHTML = congratulationWinner;
 }
 //游戏进行
 function gameOn(e) {
-        event = e;
-        var qiziPosition = getqiziPosition();
-        if(checkIfinBoard(qiziPosition)&&checkNull(qiziPosition)) {
-            move(qiziPosition);
-            tip();
-        }else {
-            warning();
-        }
-       if(winner){
-           gameOver();
-       }
+    event = e;
+    var qiziPosition = getqiziPosition();
+    if (checkIfinBoard(qiziPosition) && checkNull(qiziPosition)) {
+        move(qiziPosition);
+        tip();
+    } else {
+        warning();
+    }
+    if (winner) {
+        gameOver();
+    }
 }
 
 
 function takeBack() {
-       context.clearRect(lastPosition.x * interval +extra,lastPosition.y * interval + extra, chessSize,chessSize);
-       qizis[lastPosition.x][lastPosition.y] = "";
+    context.clearRect(lastPosition.x * interval + extra, lastPosition.y * interval + extra, chessSize, chessSize);
+    qizis[lastPosition.x][lastPosition.y] = "";
 }
